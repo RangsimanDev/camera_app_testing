4d76475e909717da8074e3a9a49b8156104f830d
RangsimanDev <Rangsiman.ain@gmail.com>
2024-02-12 03:22:36 +0700

    sdfksdfjklsdf

---
+++ app.js
@@ -0,0 +1,25 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraOpen = document.querySelector("#camera--open")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+// Start the video stream when the window loads
+
+cameraOpen.onclick = function () {
+    cameraStart();
+}
